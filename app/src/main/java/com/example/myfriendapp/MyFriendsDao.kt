f894823606c9e252a69882156a558a9aa061690f
Mous <rezasubkhan50@gmail.com>
2023-04-02 13:24:08 +0700

    First Always

---
+++ app/src/main/java/com/example/myfriendapp/MyFriendsDao.kt
@@ -0,0 +1,17 @@
+package com.example.myfriendapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
+interface MyFriendsDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun tambahTeman(friend: MyFriend)
+
+    @Query("SELECT * FROM MyFriend")
+    fun ambilSemuaTeman(): LiveData<List<MyFriend>>
+}
